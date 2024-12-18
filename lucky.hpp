0e3f648f32e85918a644d8a15ba0aae93a37b586
Katya <129635004+prkpeevakatya@users.noreply.github.com>
2024-12-18 05:05:38 +0200

    Create lucky.hpp

---
+++ lucky.hpp
@@ -0,0 +1,8 @@
+bool is_prime (int x);bool contains_6 (int x);
+int  create_and_fill_array(int n);void delete_array(int  A, int n);
+void print_array(int  A, int n);bool has_prime_element(int  A, int n);
+bool has_identical_columns(int  A, int n);int sum_in_row(int  A, int n, int k);
+void sort_array (int ** A, int n);
+int min_in_column(int ** A, int n, int k);
+int replace_min_in_column (int ** A, int n, int x);
+bool diagonal_elements_positive(int ** A, int n);
